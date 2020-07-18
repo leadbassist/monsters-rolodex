c4ec83561b470e49cf9dd18bd3c2abe64ef7966d
leadbassist <leadbassist@users.noreply.github.com>
2020-07-18 11:27:46 -0400

    Updates

---
+++ precache-manifest.502c3a04c3909442db448f5a00bb47e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfcec0af7328681aba8cc2403ed22e20",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "55a9c9728a6d4e5890f4",
+    "url": "/monsters-rolodex/static/css/main.a3bc1ea0.chunk.css"
+  },
+  {
+    "revision": "1d6d326f4470ed67fe85",
+    "url": "/monsters-rolodex/static/js/2.46e2921d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.46e2921d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55a9c9728a6d4e5890f4",
+    "url": "/monsters-rolodex/static/js/main.8a63859f.chunk.js"
+  },
+  {
+    "revision": "aa1aeb4ed7e62e71a38e",
+    "url": "/monsters-rolodex/static/js/runtime-main.2292ee2f.js"
+  }
+]);
